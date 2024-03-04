72a0a51fc60343142ebc0637987cfb36ecbdfa2e
arthurnxtto <arthurvnetto8@outlook.com>
2024-03-04 20:21:26 -0300

    exemplo 02 php

---
+++ exemplo02.php
@@ -0,0 +1,16 @@
+<?php 
+	//declarando variaveis e inicializando
+	$valor1 = 40.5;
+	$valor2 = 20;
+	$nome = "Jonathan";
+	$cidade = 'São Paulo';
+	$flag = true;
+	$status = false;
+
+	echo "$valor1 <br />";
+	echo "$valor2 <br />";
+	echo "$nome <br />";
+	echo "$cidade <br />";
+	echo "$flag <br />";
+	echo "$status <br />";
+ ?>
